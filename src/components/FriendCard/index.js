5a329f7688f50a71c7fb2da4582ae74ada141554
Greg D <gregorydesmarais@gmail.com>
2019-11-25 16:14:43 -0500

    Prepping FriendCard for additional props

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -5,11 +5,8 @@ function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+          <img alt={props.name} src={props.image}/>
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
     </div>
   );
 }
