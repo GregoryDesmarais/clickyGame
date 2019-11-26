210340c2dce8a0995dc50e603ca31bb29c9c06d1
Greg D <gregorydesmarais@gmail.com>
2019-11-26 11:03:21 -0500

    Updates

---
+++ precache-manifest.acaece94a9d442e7eef86cfc6730466e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a24faef0845fa1a42a5cb98552218a",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "b9005ab34782941bf1f7",
+    "url": "/clickyGame/static/css/main.dca15d48.chunk.css"
+  },
+  {
+    "revision": "c7718f706d1fa03d0878",
+    "url": "/clickyGame/static/js/2.0f377ec7.chunk.js"
+  },
+  {
+    "revision": "b9005ab34782941bf1f7",
+    "url": "/clickyGame/static/js/main.08f8de86.chunk.js"
+  },
+  {
+    "revision": "b86591952698d412d003",
+    "url": "/clickyGame/static/js/runtime-main.db88dee0.js"
+  }
+]);
